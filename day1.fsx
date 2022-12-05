7aa752699698dab2437dedc165a0ef54ec5813fd
Derek <dlomax@freeus.com>
2022-12-05 09:31:14 -0700

    Reformat files and put part 1 and part 2 together

--- day1.fsx
+++ day1.fsx
@@ -1,9 +1,23 @@
 open System
 open System.IO
 
+let print_advent_information day part =
+    printfn "\n\nAdvent of code day %d, part %d results" day part
+
 let lines = File.ReadAllLines("./data/day1_data.txt")
 
+print_advent_information 1 1
+
 String.Join(" ", lines).Split("  ")
-|>Seq.map(fun text -> text.Split() |> Seq.map int |> Seq.sum)
-|>Seq.max
-|>printfn "The total calories of the elf with the most food is: %d"
+|> Seq.map (fun text -> text.Split() |> Seq.map int |> Seq.sum)
+|> Seq.max
+|> printfn "The total calories of the elf with the most food is: %d"
+
+print_advent_information 1 2
+
+String.Join(" ", lines).Split("  ")
+|> Seq.map (fun text -> text.Split() |> Seq.map int |> Seq.sum)
+|> Seq.sortDescending
+|> Seq.take 3
+|> Seq.sum
+|> printfn "The total sum of the calories of the top 3 elves is: %d"
